9693458d7831a2e0a0619ad25ce1abe9af07a589
Lawal <losthoodz22s@gmail.com>
2025-10-20 10:20:22 +0100

    Fix robots.txt conflict: use static file instead of dynamic generation

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-60074c628fb7e62b.js","static/chunks/fd9d1056-840ceeade1fc2d9e.js","static/chunks/117-e8f43bda6fc17b77.js","static/chunks/main-app-d0b71cceb230dfde.js"],pages:{"/_app":["static/chunks/webpack-60074c628fb7e62b.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-9f3ae336d79206c0.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-60074c628fb7e62b.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-9f3ae336d79206c0.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- .next/static/Py4IcORah57aNKpya1z7-/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fimport\u002F[broker]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
